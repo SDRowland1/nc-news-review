5b117e6c6ebeeace32e1bd083d87e8a2a02220be
Sam Rowland <samrowland1@Sams-MacBook-Pro.local>
2020-04-13 16:51:00 +0100

    heroku dataa

--- knexfile.js
+++ knexfile.js
@@ -1,30 +1,34 @@
-const ENV = process.env.NODE_ENV || 'development';
+const ENV = process.env.NODE_ENV || "development";
+
+const { DB_URL } = process.env;
+// ...
 
 const baseConfig = {
-  client: 'pg',
+  client: "pg",
   migrations: {
-    directory: './db/migrations'
+    directory: "./db/migrations",
   },
   seeds: {
-    directory: './db/seeds'
-  }
+    directory: "./db/seeds",
+  },
 };
 
 const customConfig = {
   development: {
     connection: {
-      database: 'nc_news'
-      // user,
-      // password
-    }
+      database: "nc_news",
+    },
+    production: {
+      connection: `${DB_URL}?ssl=true`,
+    },
+    test: {
+      connection: {
+        database: "nc_news_test",
+        // user,
+        // password
+      },
+    },
   },
-  test: {
-    connection: {
-      database: 'nc_news_test'
-      // user,
-      // password
-    }
-  }
 };
 
 module.exports = { ...customConfig[ENV], ...baseConfig };
