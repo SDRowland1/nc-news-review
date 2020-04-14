abb54eaa77fd364768f9af9ee38db1133ea5eaaa
Sam Rowland <samrowland1@Sams-MacBook-Pro.local>
2020-04-14 14:31:45 +0100

    cors update

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
 const apiRouter = require("./routers/api-router");
+const cors = require("cors");
 const {
   handlePathErrors,
   handleCustomErrors,
   handlePsqlErrors,
-  handle500s
+  handle500s,
 } = require("./errors/index");
+
+app.use(cors());
+
 app.use(express.json());
 app.use("/api", apiRouter);
 
@@ -14,4 +18,5 @@ app.use("/*", handlePathErrors);
 app.use(handleCustomErrors);
 app.use(handlePsqlErrors);
 app.use(handle500s);
+
 module.exports = app;
